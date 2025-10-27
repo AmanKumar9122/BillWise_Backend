0c62b7a5f8088c4fb154c72a908dd88f3bf895f0
AmanKumar9122 <amanraj15112003@gmail.com>
2025-10-27 22:57:41 +0530

    add User entity with role mapping and registration constructor

---
+++ src/main/java/com/aksps/BillWise/model/User.java
@@ -0,0 +1,46 @@
+package com.aksps.BillWise.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password; // Stored as hashed value (BCrypt)
+
+    @ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    @JoinTable(
+        name = "user_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    // Constructor used for registering new users
+
+    public User(String username ,String email , String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
