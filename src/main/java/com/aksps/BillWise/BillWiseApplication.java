544130cdbf3597a30acee16c9275acf1f95e6bc4
AmanKumar9122 <amanraj15112003@gmail.com>
2025-10-27 19:06:05 +0530

    Initial project setup with Spring Boot 3, PostgreSQL, and security.

---
+++ src/main/java/com/aksps/BillWise/BillWiseApplication.java
@@ -0,0 +1,13 @@
+package com.aksps.BillWise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillWiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aksps/BillWise/BillWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aksps.BillWise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
