c9e8c789bbc98cbc4a16316d3845303d67f40341
AmanKumar9122 <amanraj15112003@gmail.com>
2025-10-28 11:55:43 +0530

    add UserRepository for user data management

---
+++ src/main/java/com/aksps/BillWise/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.aksps.BillWise.repository;
+
+import com.aksps.BillWise.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // Method to find a user by their username
+    Optional<User> findByUsername(String username);
+
+    // Methods to check existence of username and email
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
