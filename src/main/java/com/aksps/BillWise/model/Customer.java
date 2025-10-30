844409b0749a203633ab30956f789766a7100126
AmanKumar9122 <amanraj15112003@gmail.com>
2025-10-30 16:18:54 +0530

    add Customer entity

---
+++ src/main/java/com/aksps/BillWise/model/Customer.java
@@ -0,0 +1,25 @@
+package com.aksps.BillWise.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Customer {
+    @Id
+    @GeneratedValue (strategy =GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String contactNumber;
+    private String email;
+
+    // optional field
+    private String gstNumber;
+}
